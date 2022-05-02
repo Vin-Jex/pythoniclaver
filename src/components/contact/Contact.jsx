3436c1cc115298029d6e9f670fa58c74e2ac48c0
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-05-02 21:00:54 +0100

    update file

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
 import './contact.css';
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
   return (
     <section id='contact'>
-      <h5></h5>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon' />
+            <h4>Email</h4>
+            <h5>vincentcode0@gmail.com</h5>
+            <a href="mailto: vincentcode0@gmail.com" target="_blank" rel='noreferrer noopener'>Send a message</a>
+          </article>
+
+          <article className='contact__option'>
+            <RiMessengerLine className='contact__option-icon' />
+            <h4>Messenger</h4>
+            <h5>Pythonichub</h5>
+            <a href="https://m.me/vincent.coder" target="_blank" rel='noreferrer noopener'>Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>+234 906 8985 453</h5>
+            <a href="https://api.whatsapp.com/send?phone+2349068985453" target="_blank" rel='noreferrer noopener'>Send a message</a>
+          </article>
+        </div>
+        
+        {/* END OF CONTACT OPTIONS */}
+
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button> 
+        </form>
+        
+      </div>
     </section>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './footer.css'
-
+import {FaFacebook} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
+ 
 const Footer = () => {
   return (
-    <section id='footer'>Footer</section>
+    <footer id='footer'>
+      <a href="#" className="footer__logo">PYTHONICHUB</a>
+
+        <ul className="permalinks">
+          <li><a href="#">HOME</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#expereince">Expereince</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#testimonials">Testimonials</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      
+      <div className="footer__socials">
+        <a href="https://facebook.com"><FaFacebook /></a>
+        <a href="https://instagram.com"><FiInstagram /></a>
+        <a href="https://twitter.com"><FiInstagram /></a>
+      </div>
+    </footer>
   )
 }
 
