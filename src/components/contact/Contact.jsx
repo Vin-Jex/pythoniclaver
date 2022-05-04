24b1ee48e491eb9a9290296589c2fe3134df13ab
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-05-04 13:11:17 +0100

    update file

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,18 @@ import './contact.css';
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
+
 
 const Contact = () => {
+  const form = useRef(); 
+  
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(' service_3kxu1at ', ' service_3kxu1at ', form.current, ' ')   
+  };
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -30,13 +40,13 @@ const Contact = () => {
             <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+234 906 8985 453</h5>
-            <a href="https://api.whatsapp.com/send?phone+2349068985453" target="_blank" rel='noreferrer noopener'>Send a message</a>
+            <a href="https://api.whatsapp.com/send/?phone=+2349068985453&text=MyTextMessage" target="_blank" rel='noreferrer noopener'>Send a message</a>
           </article>
         </div>
         
         {/* END OF CONTACT OPTIONS */}
 
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
