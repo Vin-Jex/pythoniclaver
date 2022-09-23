411a56ee2df2857f95b47db3b761b82cbfb47b60
OKEREKE VINCENT <Vincentcode0@gmail.com>
2022-09-23 02:30:12 +0100

    Change Nav Icon and Navigation location

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './nav.css' 
 import {AiOutlineHome} from 'react-icons/ai'
-import {AiOutlineUser} from 'react-icons/ai'
+import {FaScroll} from 'react-icons/fa'
 import {BiBook} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageSquareDetail} from 'react-icons/bi'
@@ -14,14 +14,14 @@ export default function Nav() {
         <a href="#"  onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}>
           <AiOutlineHome />
         </a>
-        <a href="#about"  onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''} >
-          <AiOutlineUser />
-        </a>
         <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''} >
           <BiBook />
         </a>
         <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''} >
           <RiServiceLine />
+        </a>
+        <a href="#portfolio"  onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''} >
+        <FaScroll />
         </a>
         <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''} >
           <BiMessageSquareDetail />
