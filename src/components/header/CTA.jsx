67beaed621e73b9da3fcf2b160778e90008c8d02
Vincent <98766202+pythoniclaver@users.noreply.github.com>
2022-09-29 19:02:22 +0100

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href='https://drive.google.com/file/d/14yKqWxiFMCH948gAs5YdCBzsyFm_EctH/view'
+        <a href='https://drive.google.com/file/d/1ZG47wI6Pb3QkYcYuF6OKoZF2pzMIhZac/view'
         target="_blank"
         rel='noreferrer'
         className='btn'>Download CV</a>
