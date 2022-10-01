ba4b34311372b33f0ca52373b2e41d7cc488d154
Vincent <98766202+pythoniclaver@users.noreply.github.com>
2022-10-01 11:52:48 +0100

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -6,7 +6,7 @@ const CTA = () => {
         <a href='https://drive.google.com/file/d/1_Hb4d9FqsWkWF62pylpCFr6bdvW-d1Et/view'
         target="_blank"
         rel='noreferrer'
-        className='btn'>MY CV</a>
+        className='btn'>HIRE ME</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
