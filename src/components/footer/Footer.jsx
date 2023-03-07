37c18bcc7b639739b49b0bd8e9ef5569e8aabc1f
OKEREKE VINCENT <vincentcode0@gmail.com>
2023-03-07 01:24:30 +0100

    feat: Update contact

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,35 +1,68 @@
 import React from 'react'
 import './footer.css'
-import {FaFacebook} from 'react-icons/fa'
-import {FiInstagram} from 'react-icons/fi'
 import {IoLogoTwitter} from 'react-icons/io'
+import { BsLinkedin, BsYoutube } from 'react-icons/bs'
  
 const Footer = () => {
   return (
     <footer id='footer'>
-      <a href="#" className="footer__logo">PYTHONICHUB</a>
+      <a href='#' className='footer__logo'>
+        PYTHONICHUB
+      </a>
 
-        <ul className="permalinks">
-          <li><a href="#">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#experience">Experience</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#testimonials">Testimonials</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      
-      <div className="footer__socials">
-        <a href="https://facebook.com/Vincent.coderr/" target="_blank" rel='noreferrer noopener'><FaFacebook /></a>
-        <a href="https://instagram.com/pythonic.lava" target="_blank" rel='noreferrer noopener'><FiInstagram /></a>
-        <a href="https://twitter.com/Vincent_coder" target="_blank" rel='noreferrer noopener'><IoLogoTwitter /></a>
+      <ul className='permalinks'>
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#about'>About</a>
+        </li>
+        <li>
+          <a href='#experience'>Experience</a>
+        </li>
+        <li>
+          <a href='#services'>Services</a>
+        </li>
+        <li>
+          <a href='#portfolio'>Portfolio</a>
+        </li>
+        <li>
+          <a href='#testimonials'>Testimonials</a>
+        </li>
+        <li>
+          <a href='#contact'>Contact</a>
+        </li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a
+          href='https://twitter.com/Vincent_coder'
+          target='_blank'
+          rel='noreferrer noopener'
+        >
+          <IoLogoTwitter />
+        </a>
+        <a
+          href='https://www.linkedin.com/in/pythonichub'
+          target='_blank'
+          rel='noreferrer noopener'
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href='https://www.youtube.com/channel/UCBfE8bZceHtpxg5ALYT0etw'
+          target='_blank'
+          rel='noreferrer noopener'
+        >
+          <BsYoutube />
+        </a>
       </div>
 
-    <div className="footer__copyright">
-      <small>&copy; PYTHONICHUB. All rights reserved</small>
-    </div>
+      <div className='footer__copyright'>
+        <small>&copy; PYTHONICHUB. All rights reserved</small>
+      </div>
     </footer>
-  )
+  );
 }
 
 export default Footer;
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import {BsLinkedin} from 'react-icons/bs'
+import {BsLinkedin, BsTwitter} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
 import {FiDribbble} from 'react-icons/fi'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a target="_blank" rel="noreferrer noopener" href="https://linkedin.com/in/pythonichub" >
-            <BsLinkedin />
-        </a>
-        <a target="_blank" rel="noreferrer noopener" href="https://github.com/pythoniclaver">
-            <FaGithub />
-        </a>
-        <a target="_blank" href="https://dribbble.com" rel="noreferrer">
-            <FiDribbble />
-        </a>
+      <a
+        target='_blank'
+        rel='noreferrer noopener'
+        href='https://linkedin.com/in/pythonichub'
+      >
+        <BsLinkedin />
+      </a>
+      <a
+        target='_blank'
+        rel='noreferrer noopener'
+        href='https://github.com/pythoniclaver'
+      >
+        <FaGithub />
+      </a>
+      <a target='_blank' href='https://twitter.com/vincent_coder' rel='noreferrer'>
+        <BsTwitter />
+      </a>
     </div>
-  )
+  );
 }
 
 export default HeaderSocials;
